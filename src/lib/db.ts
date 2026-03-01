f57bddc93273be1c03940db781e99df8ad438fd3
b.bryxnnn <bryanong.2551@gmail.com>
2026-03-01 21:25:12 +0700

    fix: provide fallback DB URL & restore url in schema

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '@/generated/prisma/index.js'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
+}
+
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "postgresql://dummy:dummy@localhost:5432/dummy"
 }
 
 export const db = globalForPrisma.prisma ?? new PrismaClient()
